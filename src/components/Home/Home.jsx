67c8537249f98facc050112b05be5f01b6bdf2f1
recipra <jmo0425@outlook.com>
2022-08-25 14:15:51 -0400

    add tablet media queries

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import styles from './Home.module.css'
 import Me from '../../assets/pfp-cropped.jpg'
+import CoolerMe from '../../assets/cooler-pfp.jpg'
 
 const Home = () => {
   return (
@@ -7,7 +8,8 @@ const Home = () => {
       <div className={styles.contentContainer}>
         <h3>Full Stack Software Engineer/Web Developer</h3>
         <div className={styles.pfpContainer}>
-          <img src={Me} alt="Too much swag" className={styles.pfp} />
+          <img src={Me} alt="Mobile pfp" className={styles.pfp} />
+          <img src={CoolerMe} alt="Desktop/tablet pfp" className={styles.coolerPfp} />
         </div>
       </div>
     </>
