538d47cca0f10b5bacd504c318844555728b899c
recipra <jmo0425@outlook.com>
2022-08-25 13:40:43 -0400

    adjust text color

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = ({ handleHomeClick, handleAboutClick, handleContactClick, handleP
         <ul>
           <li><button onClick={handleHomeClick}>Home</button></li>
           <li><button onClick={handleAboutClick}>About</button></li>
-          <li><button onClick={handleContactClick}>Contact Me</button></li>
+          <li><button onClick={handleContactClick}>Contact</button></li>
           <li><button onClick={handleProjectsClick}>Projects</button></li>
         </ul>
         <h3 className={styles.resumeLink}><a href="https://docs.google.com/document/d/1ef4aJaHu2QDmkyktrtzqIbG5peDOv8LknlK9u0oJs4Q" target='_blank' rel='noreferrer noopener'>Resume</a></h3>
